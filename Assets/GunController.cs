3bae654507ae10d28f379cd9098b37456052f5b5
Liam Sutton <liamsutton1997@outlook.com>
2021-03-11 13:56:19 +0000

    Can now fire laser from gun barrel position

---
+++ Assets/GunController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform barrelPoint;
+    private LineRenderer lineRenderer;
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRenderer.SetPosition(0, barrelPoint.position);
+        lineRenderer.SetPosition(1, barrelPoint.transform.forward * 100.0f);
+    }
+}
