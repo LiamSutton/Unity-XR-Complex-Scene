7f685b02ae4b390f914b34a30fd2d18866874993
Liam Sutton <liamsutton1997@outlook.com>
2021-03-12 11:02:14 +0000

    added teleport menu, teleporting back to main stage doesnt work though

---
+++ Assets/Scripts/TeleportMenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportMenuController : MonoBehaviour
+{
+    public GameObject player;
+    public Transform mainStagePosition;
+    public Transform shootingGalleryStagePosition;
+
+    public void TeleportToMainStage()
+    {
+        player.transform.position = mainStagePosition.position;
+        player.transform.rotation = mainStagePosition.rotation;
+    }
+
+    public void TeleportToShootingGallery() {
+        player.transform.position = shootingGalleryStagePosition.position;
+        player.transform.rotation = shootingGalleryStagePosition.rotation;
+    }
+}
