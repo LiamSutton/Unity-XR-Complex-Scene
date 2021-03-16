8cb8c5d0b8dd3a26b16d6bb066fa077c67dc6425
Liam Sutton <liamsutton1997@outlook.com>
2021-03-16 14:03:11 +0000

    added level geometry for Cube spawner and hooked up teleporter

--- Assets/Scripts/TeleportMenuController.cs
+++ Assets/Scripts/TeleportMenuController.cs
@@ -8,14 +8,24 @@ public class TeleportMenuController : MonoBehaviour
     public Transform mainStagePosition;
     public Transform shootingGalleryStagePosition;
 
+    public Transform cubeSpawnerStagePosition;
+
     public void TeleportToMainStage()
     {
         player.transform.position = mainStagePosition.position;
         player.transform.rotation = mainStagePosition.rotation;
+        Debug.Log("Teleporting to Main Stage");
     }
 
     public void TeleportToShootingGallery() {
         player.transform.position = shootingGalleryStagePosition.position;
         player.transform.rotation = shootingGalleryStagePosition.rotation;
+        Debug.Log("Teleporting to Shooting Gallery");
+    }
+
+    public void TeleportToCubeSpawner() {
+        player.transform.position = cubeSpawnerStagePosition.position;
+        player.transform.rotation = cubeSpawnerStagePosition.rotation;
+        Debug.Log("Teleporting to Cube Spawner");
     }
 }
