204047138ebfa42e33757fec7d84f9a2b8943f67
Liam Sutton <liamsutton1997@outlook.com>
2021-03-16 16:24:25 +0000

    Cube spawner now spawns and resets when necessary

---
+++ Assets/Scripts/CubeSpawnerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CubeSpawnerController : MonoBehaviour
+{
+    public GameObject cubePrefab;
+
+    public GameObject spawnButton;
+
+    public GameObject resetButton;
+
+    public Transform spawnPoint;
+
+    private float spawnDelay = 0.1f;
+
+    public int n;
+    void Start()
+    {
+
+    }
+
+    public void SpawnButtonPressed() {
+        spawnButton.SetActive(false);
+        StartCoroutine("BeginSpawning");
+    }
+    public IEnumerator BeginSpawning()
+    {
+        Debug.Log("Spawning " + this.n.ToString() + " Cubes.");
+        for (int i = 0; i <= this.n; i++)
+        {
+            SpawnCube();
+            yield return new WaitForSeconds(spawnDelay);
+        }
+        resetButton.SetActive(true);
+    }
+
+    public void Reset()
+    {
+        GameObject[] cubes = GameObject.FindGameObjectsWithTag("Spawned");
+
+        for (int i = 0; i < cubes.Length; i++) {
+            DestroyImmediate(cubes[i]);
+        }
+        resetButton.SetActive(false);
+        spawnButton.SetActive(true);
+    }
+
+    public void SetCubeAmount(int n)
+    {
+        this.n = n;
+    }
+
+    private void SpawnCube()
+    {
+        Instantiate(cubePrefab, spawnPoint.position, Quaternion.identity);
+    }
+}
